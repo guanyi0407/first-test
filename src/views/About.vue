1f36742ec6b832d58ffbdbd053136411e00d7298
Billy <guanyiji0407@gmail.com>
2021-10-03 10:58:57 +0800

    init

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="container">
+      <h1>This is an about page</h1>
+      <p>123 TEST</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        animi doloremque vitae tempore corporis, ipsa aperiam reiciendis est
+        accusantium repellat quibusdam atque eum dolore, quasi enim
+        necessitatibus explicabo, ullam omnis facilis. Non natus veniam
+        doloremque voluptatem beatae incidunt unde eaque. Quia non saepe tempora
+        necessitatibus libero explicabo dolor, fugiat adipisci?
+      </p>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.about {
+  .container {
+    width: 1000px;
+    margin: 0 auto;
+  }
+  p {
+    color: #3ca5e6;
+    font-size: 22px;
+    font-weight: 700;
+  }
+}
+</style>
